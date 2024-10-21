69425703ce39c8262e101a5345de7845b52e218b
andradesysadmin <64712751+andradesysadmin@users.noreply.github.com.>
2024-10-20 23:07:51 -0400

    update

--- start.sh
+++ start.sh
@@ -1,35 +1,75 @@
 #!/bin/bash
 
-#   Adicione permissão de execução a este script com chmod +x start.sh
-#   Execute esse script com root ou usando sudo
 
-# Adiciona permissão de execulsão a todos os outros scripts
+
+
+#############################################################################
+#                                                                           #  
+#   Adicione permissão de execução a este script com chmod +x start.sh      #
+#   Execute esse script com root ou usando sudo                             #
+#                                                                           #
+#############################################################################
+
+
+
+
+# Valida se o script foi executado com os devidos privilégios
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "Este script precisa ser executado como root"
     exit 1
 fi
 
-# Tranforma todos os scripts em executaveis
+
+# Tranforma todos os scripts do projeto em executaveis
 chmod +x */*.sh
 
-function installs(){
 
-    sudo /installs/docker_install.sh
-    sudo /installs/python_install.sh
-    sudo /installs/jenkins_install.sh
+#Função que instala as aplicações 
+function installs() {
+
+    # Loop para execultar scripts contidos installs/ e validar suas saidas
+    DIR = $(ls installs/)
+    for in f in $DIR; do
+
+        sudo /installs/$f
+        if [ $? -eq 0 ]; then
+
+            echo "$f executado com sucesso."
+        else
+
+            echo "Erro ao executar $f"
+            exit 1
+        fi
+    done
 
 }
 
 function apps(){
 
+    # Loop para execultar scripts contidos apps/ e validar suas saidas
+    # -r para instalar o postgres primeiro
+    DIR = $(ls -r apps/)
+    for in f in $DIR; do
+
+        sudo /installs/$f
+        if [ $? -eq 0 ]; then
+
+            echo "$f executado com sucesso."
+        else
+
+            echo "Erro ao executar $f"
+            exit 1
+        fi
+    done
+
 }
 
-function automation(){
+# function automation(){
 
-}
+# }
 
 # Chama as funções
 installs
 apps
-automation
+#automation
